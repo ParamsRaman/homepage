374c8a2da8794e7fb76fe46839a3aa90de1b9f5e
Params <parameshr@gmail.com>
2014-10-27 23:51:35 -0700

    second commit

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+#To build and run the website locally, uncomment the below two lines:
+#jekyll build
+#jekyll serve
+
+
+#To build and deploy the website to a remote server (modify the server details as per your needs), uncomment the below two lines:
 jekyll build
-jekyll serve
-#rsync -vaz _site/ praman1@unix.ic.ucsc.edu:public_html
+rsync -vaz _site/ praman1@unix.ic.ucsc.edu:public_html
