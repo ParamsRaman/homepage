f3127d460640f78dd2297f70fce906df3532da9a
Params <params@params-macbookpro.local>
2014-11-17 05:04:51 -0800

    updated post

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 #To build and run the website locally, uncomment the below two lines:
 #jekyll build
 #jekyll serve
+#open -a "Google Chrome" "http://127.0.0.1:4000/~praman1"
 
 
 #To build and deploy the website to a remote server (modify the server details as per your needs), uncomment the below two lines:
